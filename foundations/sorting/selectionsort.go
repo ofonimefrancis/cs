fc3eb63506dd37944d7c528dd8ea189f4cb18846
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-04-22 12:05:05 +0200

    Prints for visualization purposes

--- foundations/sorting/selectionsort.go
+++ foundations/sorting/selectionsort.go
@@ -1,13 +1,17 @@
 package sorting
 
+import "fmt"
+
 //Selection - Selection Sort
 func Selection(dataSet []int) []int {
+	fmt.Println(dataSet)
 	for i := 0; i < len(dataSet); i++ {
 		for j := i + 1; j < len(dataSet); j++ {
 			if dataSet[j] < dataSet[i] {
 				dataSet[i], dataSet[j] = dataSet[j], dataSet[i]
 			}
 		}
+		fmt.Println(dataSet)
 	}
 	return dataSet
 }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ofonimefrancis/cs-fundamentals/foundations/sorting"
 )
 
 func main() {
-	data := []int{5, 10, 1, 3, 9, 2, 6}
-	fmt.Println(sorting.Selection(data))
+	data := []int{5, 2, 4, 6, 1, 3}
+	sorting.Selection(data)
 }
